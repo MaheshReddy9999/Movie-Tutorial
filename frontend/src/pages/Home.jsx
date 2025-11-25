2a5e9218ba8302cb33c55c9db0478373e4790001
MaheshReddy9999 <maheshcallingmahesh@gmail.com>
2025-11-25 15:24:25 -0600

    new movies added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,11 @@ function Home() {
     const [searchQuery, setSearchQuery] = useState("");
 
     const movies = [
-       { id: 1, title: "Bahubali", release_date: "2015", image: "bahubali.jpg" },
-       { id: 2, title: "Mirchi", release_date: "2014",  image: "mirchi.jpg" },
-       { id: 3, title: "Bahubali-2", release_date: "2018",  image: "bahubali2.jpg" },
+       { id: 1, title: "Bahubali", release_date: "2015", image: "bahubalinew.jpg" },
+       { id: 2, title: "Mirchi", release_date: "2013",  image: "mirchi.jpg" },
+       { id: 3, title: "Bahubali-2", release_date: "2017",  image: "bahubali2.jpg" },
+       { id: 4, title: "Saaho", release_date: "2018",  image: "saaho.jpg" },
+       { id: 5, title: "Salaar", release_date: "2023",  image: "salaar.jpg" },
   ];
 
     const handleSearch = (e) => {
